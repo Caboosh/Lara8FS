dc96a1f6b18cb6d2563f73ea53bbd98c96b9302c
BuildTools <unconfigured@null.spigotmc.org>
2021-09-27 15:46:23 +0100

    start converting static html theme

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
-        <h4>{{ $post->excerpt }}</h4><h4>Published on: {{ date('F j, Y',$post->date) }} </h4>
+        <h4>{{ $post->excerpt }}</h4><h4>Published on: {{ date('F j, Y',$post->date) }}, By: <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a></h4>
         <h4>Category: <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h4>
 
-         {!! $post->body !!}
+         <p>{!! $post->body !!}</p>
 
         <a href="/">Go Back</a>
     </article>
